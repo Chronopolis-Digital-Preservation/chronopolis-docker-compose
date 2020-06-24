266037e8f895994b6e4ac325f42567dc5c9c2d21
Thor Brickman <tnbrickman@ucsd.edu>
2020-06-24 14:39:13 -0700

    More mods to build sequence.

--- chronopolishome/chronopolisbuild.sh
+++ chronopolishome/chronopolisbuild.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 chown -R chronopolis:chronopolis /home/chronopolis
+mkdir -p /export/outgoing/bags
+mkdir -p /export/outgoing/tokens
+chown -R chronopolis:chronopolis /export
 
 su chronopolis <<ENDOFSU
-  # Things you might want to do as chronopolis when you bring up the build env.
+  # Do things as chronopolis when the build environment comes up.
 ENDOFSU 
 
 while true; do
