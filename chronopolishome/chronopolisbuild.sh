69fc7cb7708d4702eac8013dcb3f217d1b6bdbbd
Thor Brickman <tnbrickman@gmail.com>
2020-06-26 15:35:53 -0700

    Changing permissions for chronopolis-core source tree in build container.

--- chronopolishome/chronopolisbuild.sh
+++ chronopolishome/chronopolisbuild.sh
@@ -4,6 +4,7 @@ chown -R chronopolis:chronopolis /home/chronopolis
 mkdir -p /export/outgoing/bags
 mkdir -p /export/outgoing/tokens
 chown -R chronopolis:chronopolis /export
+chown -R chronopolis:chronopolis /chronopolis-core
 
 su chronopolis <<ENDOFSU
   # Do things as chronopolis when the build environment comes up.
