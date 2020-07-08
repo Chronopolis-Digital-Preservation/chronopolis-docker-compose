1e46a8dd50c6d291c97a7a97811bd2320997178b
Thor Brickman <tnbrickman@gmail.com>
2020-07-08 07:47:11 -0700

    Still more key material permissions...

--- artifacts/chronopolishome/startreplication.sh
+++ artifacts/chronopolishome/startreplication.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 chown -R chronopolis:chronopolis /scratch1
 chown -R chronopolis:chronopolis /export
+chown -R chronopolis:chronopolis /home/chronopolis
+chmod -R 755 /home/chronopolis/.ssh
+chmod 740 /home/chronopolis/.ssh/*
+chmod 700 /home/chronopolis/.ssh/id_rsa
+
 su - chronopolis <<ENDOFSU
 cd /usr/local/chronopolis/replication
 /usr/bin/java -Dspring.config.location=file:/usr/local/chronopolis/replication/application.yml -jar /chronopolis-core/replication-shell/target/replication-shell-3.3.0-SNAPSHOT.jar
 ENDOFSU
-
-/usr/sbin/sshd
 
 while true; do
     sleep 30
