c4a43d214dd2eeb0add36a2ec27bcf6d0b5c9864
Thor Brickman <tnbrickman@ucsd.edu>
2020-07-09 21:53:57 +0000

    Update buildenv.sh ro -R instead of -r

--- build/buildenv.sh
+++ build/buildenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp --no-preserve=ownership -r ../artifacts/chronopolishome ..
-cp --no-preserve=ownership -r ../artifacts/postgresql ..
+cp --no-preserve=ownership -R ../artifacts/chronopolishome ..
+cp --no-preserve=ownership -R ../artifacts/postgresql ..
 mkdir -p ../export/outgoing/bags
 mkdir -p ../export/outgoing/tokens
 mkdir -p ../scratch1/chronopolis-preservation
