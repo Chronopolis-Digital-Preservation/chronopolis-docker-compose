1b615eae11e6fff68d2030fe0713ba1ae2898b0a
Thor Brickman <tnbrickman@ucsd.edu>
2020-10-12 20:28:05 +0000

    Update buildenv.sh

--- build/buildenv.sh
+++ build/buildenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp --no-preserve=ownership -R ../artifacts/chronopolishome ..
-cp --no-preserve=ownership -R ../artifacts/postgresql ..
+cp -R ../artifacts/chronopolishome ..
+cp -R ../artifacts/postgresql ..
 mkdir -p ../export/outgoing/bags
 mkdir -p ../export/outgoing/tokens
 mkdir -p ../scratch1/chronopolis-preservation
