60e687d81ce0a06c09102d4cf0e6a8a8953fa04e
Thor Brickman <tnbrickman@ucsd.edu>
2020-10-12 21:49:37 +0000

    Update buildenv.sh

--- build/buildenv.sh
+++ build/buildenv.sh
@@ -5,4 +5,4 @@ mkdir -p ../export/outgoing/bags
 mkdir -p ../export/outgoing/tokens
 mkdir -p ../scratch1/chronopolis-preservation
 mkdir -p ../postgresql/data
-chmod 755 ../postresql/docker-entrypoint-initdb.d/ini-user-db.sh
+chmod 755 ../postresql/docker-entrypoint-initdb.d/init-user-db.sh
