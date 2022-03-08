24313d84c8562e60e141a91a62cbaaeab5a8f00a
thorucsd <59933000+thorucsd@users.noreply.github.com>
2022-03-08 10:44:27 -0800

    Update buildimages.sh

--- build/buildimages.sh
+++ build/buildimages.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-docker pull uazlibraries/ace-audit-manager:1.12
-docker pull uazlibraries/ace-integrity-management:1.12
-docker pull uazlibraries/ace-dbstore-mysql:1.12
 docker pull dpage/pgadmin4
 docker pull postgres:12.2
 docker build --network host --rm -t local/chron-build .
