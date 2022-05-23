cff6df7d92941d20e66fb228ae46a39e68eea3bb
thorucsd <59933000+thorucsd@users.noreply.github.com>
2022-05-23 12:22:26 -0700

    Update chronopolisbuild.sh

--- artifacts/chronopolishome/chronopolisbuild.sh
+++ artifacts/chronopolishome/chronopolisbuild.sh
@@ -5,6 +5,7 @@ mkdir -p /export/outgoing/bags
 mkdir -p /export/outgoing/tokens
 chown -R chronopolis:chronopolis /export
 chown -R chronopolis:chronopolis /chronopolis-core
+chown -R chronopolis:chronopolis /audit-control-environment
 
 su chronopolis <<ENDOFSU
   # Do things as chronopolis when the build environment comes up.
